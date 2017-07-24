6955a1cd40c02da86590197322d4e8d1f3473762
Mwiza Simbeye, (Leo Kamali) <mwizasimbeye@gmail.com>
2017-07-25 01:54:24 +0200

    Variables in TensorFlow

---
+++ 05_variables.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+x = tf.constant([[3, 5]], dtype=tf.float32)
+y = tf.constant(5, dtype=tf.float32)
+
+op = tf.Variable(x*y, dtype=tf.float32)
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    print sess.run(op)
