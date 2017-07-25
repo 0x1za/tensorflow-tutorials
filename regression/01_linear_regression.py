2a2e94d8fd1d6728e3a8f9c6a218ab30321b759c
Mwiza Simbeye, (Leo Kamali) <mwizasimbeye@gmail.com>
2017-07-25 03:46:49 +0200

    Linear regression finail

--- regression/01_linear_regression.py
+++ regression/01_linear_regression.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # define hyper parameters
 lr = 0.01
-epochs = 100
+training_epochs = 100
 
 # Training data
 x_train = np.linspace(-1, 1, 100)
@@ -13,3 +13,32 @@ y_train = 2 * x_train + np.random.randn(*x_train.shape) * 0.33
 # Uncomment the code below to preview the data
 # plt.scatter(x_train, y_train)
 # plt.show()
+
+# Define data placeholders
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Define the model
+def model(X, w):
+    return tf.mul(X, w)
+
+# Define the weights
+w = tf.Variable(0.0, name="weights")
+
+# Cost function with mean squared error
+y_model = model(X, w)
+cost = tf.reduce_mean(tf.square(Y-y_model))
+
+# Compute the loss and optimize the parameters
+training_op = tf.train.GradientDescentOptimizer(lr).minimize(cost)
+
+# Initialize all variables
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# Interate over data
+for epoch in range(training_epochs):
+    for (x, y) in zip(x_train, y_train):
+        sess.run(training_op, feed_dict={X:y, Y:y})
+
